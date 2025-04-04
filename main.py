5947c6d76ae54188dd4f2766200c984f7d5f7b3d
Pablo <pabloarrhdez@gmail.com>
2025-04-04 18:12:46 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -17,3 +17,35 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import seaborn as sns
 import contextily as ctx
+
+import src.extraccion as ext
+import src.transformacion as trs
+import src. exploracion as exp
+import src.visualizacion as vsz
+
+cities_of_interest = [
+    "Madrid", "Barcelona", "Paris", "Berlin", "Amsterdam", 
+    "Bruxelles", "Lisbon", "Vienna", "Warsaw", 
+    "Budapest", "Stockholm", "Helsinki", "Oslo", "London", "Prague", "Dublin", "Zurich", 
+    "Munich"]
+df_sucio = pd.read_csv("Datos/bici_publicas_oficial_sucio.csv")
+df_aire_sucio = pd.read_csv("Datos/dim_aire_sucio.csv")
+df_poblacion_sucio = pd.read_csv("Datos/dim_poblacion_sucio.csv")
+analisis_estad =pd.read_csv("Datos/df_completo.csv")
+geodataframe = gpd.read_file("Datos/bicis_publicas_limpio_geodf_p.csv")
+
+if __name__ == "__main__":
+    ext.extraccion_API(cities_of_interest)
+    trs.filas_delete(df_sucio)
+    trs.columnas_transform(df_sucio1)
+    trs.transformacion_gdtf(df_limpio)
+    trs.transformacion_aire(df_aire_sucio)
+    trs.tranformacion_poblacion(df_poblacion_sucio)
+    exp.descripcion(analisis_estad)
+    exp.contraste_hipotesis_madrid(analisis_estad)
+    vsz.matriz_correlacion(analisis_estad)
+    vsz.grafico_distribcion_londres(analisis_estad)
+    vsz.grafico_distribucion_barcelona(analisis_estad)
+    vsz.grafico_distribucion_oslo(analisis_estad)
+    vsz.grafico_modelo_regresion_madrid(analisis_estad)
+    vsz.mapa_calor_paris(geodataframe)
